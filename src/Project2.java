4188d68d334bad9e2e8e90179e85ee533df49e8e
AyeshaSaif97 <ayesha.saif13@gmail.com>
2023-09-21 13:20:18 -0400

    First Commit

---
+++ src/Project2.java
@@ -0,0 +1,15 @@
+public class Project2 {
+    /*
+    Reverse a String: Write a function to reverse a given string. For
+example, given the input "Hello", the output should be "olleH".
+     */
+    public static void main(String[] args) {
+
+
+        StringBuilder sb= new StringBuilder("Canada");
+        sb.reverse();
+        System.out.println(sb);
+
+
+    }
+}
